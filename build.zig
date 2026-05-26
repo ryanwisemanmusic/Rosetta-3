0892715e40a251d1945c944cc9afddfad266d54f
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-05-26 16:59:26 -0700

    build.zig

--- build.zig
+++ build.zig
@@ -25,6 +25,21 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const translate_sysdefs = b.addTranslateC(.{
+        .root_source_file = b.path("include/win32/Zig/sys_defines_bridge.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    if (is_macos) translate_sysdefs.addIncludePath(b.path("include/shims/macos"));
+    translate_sysdefs.addIncludePath(b.path("include/shims/win32"));
+    translate_sysdefs.addIncludePath(b.path("include"));
+
+    const sysdefs_module = b.addModule("win32_sysdefs", .{
+        .root_source_file = translate_sysdefs.getOutput(),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const zig_module = b.createModule(.{
         .root_source_file = b.path("include/win32/Zig/var_sizes.zig"),
         .target = target,
@@ -35,6 +50,7 @@ pub fn build(b: *std.Build) void {
     zig_module.addIncludePath(b.path("include/shims/win32"));
     zig_module.addIncludePath(b.path("include"));
     zig_module.addImport("windows_base", windows_base_module);
+    zig_module.addImport("win32_sysdefs", sysdefs_module);
 
     const check_step = b.step("check", "Check Rosetta 3 Zig sources");
     const zig_tests = b.addTest(.{
