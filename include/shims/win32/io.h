678e4bc05356787d87ff6dd5fe94ea594cddeb8d
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-05-28 22:05:41 -0700

    io.h shim

---
+++ include/shims/win32/io.h
@@ -0,0 +1,10 @@
+/*
+ * Shim redirect for io.h.
+ * Keep the real Windows header unmodified and provide compatibility here.
+ */
+#ifndef ROSETTA3_SHIMS_WIN32_IO_REDIRECT_H
+#define ROSETTA3_SHIMS_WIN32_IO_REDIRECT_H
+
+#include "win32/io.h"
+
+#endif /* ROSETTA3_SHIMS_WIN32_IO_REDIRECT_H */
