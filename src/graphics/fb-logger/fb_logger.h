35adb9dc6abafd35bbfce936ed0b865eb8b7c34e
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-01 17:37:39 -0700

    Frame buffer logger
    Lets me capture footage from the frame buffer so that it's easier to see what is going wrong since it can be tricky to communicate problems, helpful for if your x86 program tanks real early and you need to see jpeg files of why

---
+++ src/graphics/fb-logger/fb_logger.h
@@ -0,0 +1,9 @@
+#ifndef ROSETTA3_FB_LOGGER_H
+#define ROSETTA3_FB_LOGGER_H
+
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+void rosetta3_fb_logger_capture_view(NSView *view);
+#endif
+
+#endif
---
+++ src/graphics/fb-logger/fb_logger.m
@@ -0,0 +1,57 @@
+#import <Cocoa/Cocoa.h>
+#include <stdatomic.h>
+
+#include <game/debug_runtime.h>
+#import "fb_logger.h"
+
+static dispatch_queue_t rosetta3_fb_logger_queue(void)
+{
+    static dispatch_queue_t queue;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        queue = dispatch_queue_create("rosetta3.fb_logger", DISPATCH_QUEUE_SERIAL);
+    });
+    return queue;
+}
+
+void rosetta3_fb_logger_capture_view(NSView *view)
+{
+    if (!view || !rosetta3_fb_logger_enabled()) return;
+
+    static atomic_uint_fast64_t frame_index = 0;
+    static atomic_int pending_jobs = 0;
+
+    if (atomic_load(&pending_jobs) > 2) return;
+
+    NSRect bounds = [view bounds];
+    if (bounds.size.width <= 0.0 || bounds.size.height <= 0.0) return;
+
+    NSBitmapImageRep *rep = [view bitmapImageRepForCachingDisplayInRect:bounds];
+    if (!rep) return;
+    [view cacheDisplayInRect:bounds toBitmapImageRep:rep];
+
+    CGImageRef cg_image = [rep CGImage];
+    if (!cg_image) return;
+
+    NSBitmapImageRep *copy = [[NSBitmapImageRep alloc] initWithCGImage:cg_image];
+    if (!copy) return;
+
+    uint64_t frame = atomic_fetch_add(&frame_index, 1);
+    atomic_fetch_add(&pending_jobs, 1);
+
+    NSString *dir = [NSString stringWithUTF8String:rosetta3_fb_logger_directory()];
+    dispatch_async(rosetta3_fb_logger_queue(), ^{
+        @autoreleasepool {
+            NSDictionary *props = @{
+                NSImageCompressionFactor: @0.85f,
+            };
+            NSData *jpg = [copy representationUsingType:NSBitmapImageFileTypeJPEG properties:props];
+            if (jpg && dir) {
+                NSString *file = [dir stringByAppendingPathComponent:
+                    [NSString stringWithFormat:@"frame_%06llu.jpg", frame]];
+                [jpg writeToFile:file atomically:YES];
+            }
+        }
+        atomic_fetch_sub(&pending_jobs, 1);
+    });
+}
