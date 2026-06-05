d8380f1d68c4e1b579502e5e9574678bc587656a
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 21:53:53 -0700

    snax86

--- app_testing/snax86/snax86.c
+++ app_testing/snax86/snax86.c
@@ -3,11 +3,11 @@
 #include "win32/Zig/zig_bridge.h"
 
 // This will be exported from our Zig implementation
-extern void rosetta3_run_snax86(void);
+extern void rosette_run_snax86(void);
 
 int main(int argc, char **argv) {
-    rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching snax86 (x86 ASM translated to Zig)...\n");
-    rosetta3_run_snax86();
+    rosette_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
+    printf("Rosette: Launching snax86 (x86 ASM translated to Zig)...\n");
+    rosette_run_snax86();
     return 0;
 }
