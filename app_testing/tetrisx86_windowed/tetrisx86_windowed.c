6ce8ae51e267ecbeb71e52d959cee48434eab957
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 21:51:14 -0700

    tetrisx86_windowed

--- app_testing/tetrisx86_windowed/tetrisx86_windowed.c
+++ app_testing/tetrisx86_windowed/tetrisx86_windowed.c
@@ -13,9 +13,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern void rosetta3_run_tetrisx86_core(void);
-extern void rosetta3_gfx_scene_set_canvas_size(unsigned int width, unsigned int height);
-extern void rosetta3_windowed_run(int grid_w, int grid_h,
+extern void rosette_run_tetrisx86_core(void);
+extern void rosette_gfx_scene_set_canvas_size(unsigned int width, unsigned int height);
+extern void rosette_windowed_run(int grid_w, int grid_h,
                                    int block_w, int block_h,
                                    const char *title,
                                    void (*game_func)(void *),
@@ -27,16 +27,16 @@ extern void rosetta3_windowed_run(int grid_w, int grid_h,
 static void game_thread(void *arg)
 {
     (void)arg;
-    rosetta3_run_tetrisx86_core();
+    rosette_run_tetrisx86_core();
 }
 
 int main(int argc, char **argv)
 {
-    rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching tetrisx86 windowed...\n");
-    rosetta3_gfx_scene_set_canvas_size(520, 400);
+    rosette_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
+    printf("Rosette: Launching tetrisx86 windowed...\n");
+    rosette_gfx_scene_set_canvas_size(520, 400);
     /* This title renders through the scene path; forcing the legacy block
      * framebuffer here causes unstable startup on macOS. */
-    rosetta3_windowed_run(40, 32, 0, 0, "Tetris (Windowed)", game_thread, NULL);
+    rosette_windowed_run(40, 32, 0, 0, "Tetris (Windowed)", game_thread, NULL);
     return 0;
 }
