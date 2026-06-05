e80381e44f90e8b935b055fff380497723fd6b68
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 21:12:55 -0700

    pacman_runner.c

--- app_testing/PACMAN-x86/pacman_runner.c
+++ app_testing/PACMAN-x86/pacman_runner.c
@@ -6,7 +6,7 @@ extern void rosetta3_run_pacman_text_runner(void);
 
 int main(int argc, char **argv) {
     rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching PACMAN-x86 (MASM Irvine32 translated to Zig) with ABI validation...\n");
+    printf("Rosetta 3: Launching PACMAN-x86 (JWasm Irvine32 translated to Zig) with ABI validation...\n");
     rosetta3_run_pacman_text_runner();
     return 0;
 }
