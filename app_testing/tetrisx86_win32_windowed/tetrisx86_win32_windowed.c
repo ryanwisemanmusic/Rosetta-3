5bb66f102546038a2779dba59397f2401a4c6deb
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 21:51:38 -0700

    tetrisx86_win32_windowed

--- app_testing/tetrisx86_win32_windowed/tetrisx86_win32_windowed.c
+++ app_testing/tetrisx86_win32_windowed/tetrisx86_win32_windowed.c
@@ -10,9 +10,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern void rosetta3_run_win32_tetris_core(void);
-extern void rosetta3_gfx_scene_set_canvas_size(unsigned int width, unsigned int height);
-extern void rosetta3_windowed_run(int grid_w, int grid_h,
+extern void rosette_run_win32_tetris_core(void);
+extern void rosette_gfx_scene_set_canvas_size(unsigned int width, unsigned int height);
+extern void rosette_windowed_run(int grid_w, int grid_h,
                                    int block_w, int block_h,
                                    const char *title,
                                    void (*game_func)(void *),
@@ -24,15 +24,15 @@ extern void rosetta3_windowed_run(int grid_w, int grid_h,
 static void game_thread(void *arg)
 {
     (void)arg;
-    rosetta3_run_win32_tetris_core();
+    rosette_run_win32_tetris_core();
 }
 
 int main(int argc, char **argv)
 {
-    rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching Win32 Tetris windowed...\n");
-    rosetta3_gfx_scene_set_canvas_size(520, 400);
+    rosette_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
+    printf("Rosette: Launching Win32 Tetris windowed...\n");
+    rosette_gfx_scene_set_canvas_size(520, 400);
     /* Scene-driven Win32 Tetris: no legacy block framebuffer, only direct scene composition. */
-    rosetta3_windowed_run(40, 32, 0, 0, "Tetris (Win32 Windowed)", game_thread, NULL);
+    rosette_windowed_run(40, 32, 0, 0, "Tetris (Win32 Windowed)", game_thread, NULL);
     return 0;
 }
