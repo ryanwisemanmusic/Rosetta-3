ad985e72334bc421c664bdedddad0bc0553b94a7
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 22:04:46 -0700

    tetrisx86_win32

--- app_testing/tetrisx86_win32/tetrisx86_win32.c
+++ app_testing/tetrisx86_win32/tetrisx86_win32.c
@@ -2,11 +2,11 @@
 #include "game/debug_runtime.h"
 #include "win32/Zig/zig_bridge.h"
 
-extern void rosetta3_run_win32_tetris(void);
+extern void rosette_run_win32_tetris(void);
 
 int main(int argc, char **argv) {
-    rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching Win32 Tetris (emulated Game.asm layout)...\n");
-    rosetta3_run_win32_tetris();
+    rosette_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
+    printf("Rosette: Launching Win32 Tetris (emulated Game.asm layout)...\n");
+    rosette_run_win32_tetris();
     return 0;
 }
