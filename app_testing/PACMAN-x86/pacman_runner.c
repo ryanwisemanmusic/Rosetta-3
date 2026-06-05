5004c4131b0ea8d46bcad0ae5c5b33593641febe
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-04 21:50:48 -0700

    PACMAN-x86

--- app_testing/PACMAN-x86/pacman_runner.c
+++ app_testing/PACMAN-x86/pacman_runner.c
@@ -2,11 +2,11 @@
 #include "game/debug_runtime.h"
 #include "win32/Zig/zig_bridge.h"
 
-extern void rosetta3_run_pacman_text_runner(void);
+extern void rosette_run_pacman_text_runner(void);
 
 int main(int argc, char **argv) {
-    rosetta3_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
-    printf("Rosetta 3: Launching PACMAN-x86 (JWasm Irvine32 translated to Zig) with ABI validation...\n");
-    rosetta3_run_pacman_text_runner();
+    rosette_debug_bootstrap_from_argv((argc > 0) ? argv[0] : NULL);
+    printf("Rosette: Launching PACMAN-x86 (JWasm Irvine32 translated to Zig) with ABI validation...\n");
+    rosette_run_pacman_text_runner();
     return 0;
 }
