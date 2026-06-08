bafea32f4516474d2b591ed3aa9809f2bb42ecac
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 04:57:38 -0700

    x86 TEST Zig Code

---
+++ ISA/x86/Zig/TEST/TEST.zig
@@ -0,0 +1,3 @@
+pub const family = "TEST";
+pub const path = "TEST/TEST.inc";
+pub const source = @embedFile("../../TEST/TEST.inc");
---
+++ ISA/x86/Zig/TEST/TESTUI.zig
@@ -0,0 +1,3 @@
+pub const family = "TEST";
+pub const path = "TEST/TESTUI.inc";
+pub const source = @embedFile("../../TEST/TESTUI.inc");
