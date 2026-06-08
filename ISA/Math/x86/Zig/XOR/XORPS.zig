061d17143ce8c32c63b2a98a7b72087d05beb440
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 04:59:53 -0700

    x86 XOR Zig Code

---
+++ ISA/Math/x86/Zig/XOR/XORPS.zig
@@ -0,0 +1,35 @@
+const core = @import("../../../core.zig");
+const proofs = @import("../../../proofs.zig");
+
+pub const meta = core.InstructionMathMeta{
+    .name = "XORPS",
+    .family = "XOR",
+    .path = "XOR/XORPS.inc",
+    .source_table_path = "XOR/XORPS.inc",
+    .target_isa = .x86,
+    .operation = .documented_contract,
+    .register_model = .documented_contract,
+    .flag_model = .documented_contract,
+};
+
+pub const proof_cases = [_]proofs.ProofCase{
+    .{ .documented_contract = .{ .name = "XORPS", .path = "XOR/XORPS.inc", .encoding_count = 6, .source_path_len = 13 } },
+    .{ .documented_contract = .{ .name = "XORPS", .path = "XOR/XORPS.inc", .encoding_count = 6, .source_path_len = 13 } },
+};
+
+pub const proof_report = proofs.ProofReport{
+    .meta = meta,
+    .cases = proof_cases[0..],
+};
+
+pub fn proofReport() proofs.ProofReport {
+    return proof_report;
+}
+
+pub fn verifyProofs() !void {
+    try proofs.verifyReport(proofReport());
+}
+
+test "x86 XORPS documented-contract proofs match table metadata" {
+    try verifyProofs();
+}
