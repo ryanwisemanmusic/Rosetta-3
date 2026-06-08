0b5b3aa517f9994f7ced67b6f8e9d575e7c2d679
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 13:16:02 -0700

    Cross-Lane AVX512F Extended Operations

---
+++ lib/CLEO/AVX512F/root.zig
@@ -0,0 +1,23 @@
+pub const ADDPD = @import("ADDPD.zig");
+pub const VMOVDQA32 = @import("VMOVDQA32.zig");
+pub const VMOVDQA64 = @import("VMOVDQA64.zig");
+pub const VMOVDQU32 = @import("VMOVDQU32.zig");
+pub const VMOVDQU64 = @import("VMOVDQU64.zig");
+pub const SUBPD = @import("SUBPD.zig");
+pub const SUBPS = @import("SUBPS.zig");
+
+const types = @import("../types.zig");
+
+pub const metas = [_]types.InstructionMeta{
+    ADDPD.meta,
+    VMOVDQA32.meta,
+    VMOVDQA64.meta,
+    VMOVDQU32.meta,
+    VMOVDQU64.meta,
+    SUBPD.meta,
+    SUBPS.meta,
+};
+
+pub fn validateAll() types.SafetyError!void {
+    for (metas) |meta| try types.validateMeta(meta);
+}
