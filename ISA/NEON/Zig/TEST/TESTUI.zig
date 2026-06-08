a53507520667eae7107d12cd7f63702e81250e46
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 04:50:39 -0700

    NEON TEST Zig Code

---
+++ ISA/NEON/Zig/TEST/TESTUI.zig
@@ -0,0 +1,5 @@
+pub const family = "TEST";
+pub const path = "TEST/TESTUI.inc";
+pub const source = @embedFile("../../TEST/TESTUI.inc");
+pub const x86_path = "TEST/TESTUI.inc";
+pub const target_isa = "arm64_neon";
