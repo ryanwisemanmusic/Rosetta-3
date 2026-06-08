38b737c4e24a98a079c9c37afabe7636e632b205
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 04:43:43 -0700

    CMP.zig

---
+++ ISA/NEON/Zig/CMP/CMP.zig
@@ -0,0 +1,5 @@
+pub const family = "CMP";
+pub const path = "CMP/CMP.inc";
+pub const source = @embedFile("../../CMP/CMP.inc");
+pub const x86_path = "CMP/CMP.inc";
+pub const target_isa = "arm64_neon";
