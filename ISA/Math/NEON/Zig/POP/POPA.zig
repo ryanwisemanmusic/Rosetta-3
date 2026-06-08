fe9c940a68a63eed5ab3fdc0cfb3237c16bf6952
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 05:10:50 -0700

    NEON Pop Zig Code

---
+++ ISA/Math/NEON/Zig/POP/POPA.zig
@@ -0,0 +1,35 @@
+const core = @import("../../../core.zig");
+const proofs = @import("../../../proofs.zig");
+
+pub const meta = core.InstructionMathMeta{
+    .name = "POPA",
+    .family = "POP",
+    .path = "POP/POPA.inc",
+    .source_table_path = "POP/POPA.inc",
+    .target_isa = .neon,
+    .operation = .documented_contract,
+    .register_model = .documented_contract,
+    .flag_model = .documented_contract,
+};
+
+pub const proof_cases = [_]proofs.ProofCase{
+    .{ .documented_contract = .{ .name = "POPA", .path = "POP/POPA.inc", .encoding_count = 1, .source_path_len = 12 } },
+    .{ .documented_contract = .{ .name = "POPA", .path = "POP/POPA.inc", .encoding_count = 1, .source_path_len = 12 } },
+};
+
+pub const proof_report = proofs.ProofReport{
+    .meta = meta,
+    .cases = proof_cases[0..],
+};
+
+pub fn proofReport() proofs.ProofReport {
+    return proof_report;
+}
+
+pub fn verifyProofs() !void {
+    try proofs.verifyReport(proofReport());
+}
+
+test "neon POPA documented-contract proofs match table metadata" {
+    try verifyProofs();
+}
