476b79f32e8157fc9cb09a898592d45d02b54b13
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 22:35:47 -0700

    NEON MUL Zig Code

---
+++ ISA/NEON/Zig/MUL/MULPD.zig
@@ -0,0 +1,5 @@
+pub const family = "MUL";
+pub const path = "MUL/MULPD.inc";
+pub const source = @embedFile("../../MUL/MULPD.inc");
+pub const x86_path = "MUL/MULPD.inc";
+pub const target_isa = "arm64_neon";
