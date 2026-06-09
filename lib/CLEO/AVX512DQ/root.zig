c931635310972c59c99a08f5558a1f8869dfdad9
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-08 23:08:08 -0700

    CLEO: AVX Code gets AND Instruction

--- lib/CLEO/AVX512DQ/root.zig
+++ lib/CLEO/AVX512DQ/root.zig
@@ -2,6 +2,10 @@ pub const ORPD = @import("ORPD.zig");
 pub const ORPS = @import("ORPS.zig");
 pub const XORPD = @import("XORPD.zig");
 pub const XORPS = @import("XORPS.zig");
+pub const ANDPS = @import("ANDPS.zig");
+pub const ANDPD = @import("ANDPD.zig");
+pub const ANDNPS = @import("ANDNPS.zig");
+pub const ANDNPD = @import("ANDNPD.zig");
 
 const types = @import("../types.zig");
 
@@ -10,6 +14,10 @@ pub const metas = [_]types.InstructionMeta{
     ORPS.meta,
     XORPD.meta,
     XORPS.meta,
+    ANDPS.meta,
+    ANDPD.meta,
+    ANDNPS.meta,
+    ANDNPD.meta,
 };
 
 pub fn validateAll() types.SafetyError!void {
